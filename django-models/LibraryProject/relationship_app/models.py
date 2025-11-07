c3fe1fe34aa777bd23755beb1c12921438bf512a
myla48 <ruhamazerihun2@gmail.com>
2025-11-07 23:33:22 +0300

    Update models.py

--- django-models/LibraryProject/relationship_app/models.py
+++ django-models/LibraryProject/relationship_app/models.py
@@ -1,8 +1,7 @@
 from django.db import models
 
-class Library(models.Model):
+class Author(models.Model):
     name = models.CharField(max_length=100)
-    location = models.CharField(max_length=255)
 
     def __str__(self):
         return self.name
