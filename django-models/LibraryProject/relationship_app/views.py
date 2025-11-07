ebb06e79cf11af7b894c190cf18eca715e5f6f80
myla48 <ruhamazerihun2@gmail.com>
2025-11-07 12:58:23 +0300

    Update views.py

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from django.views.generic import DetailView
-from .models import Book, Library  # Make sure Library is imported
+from .models import Book, Library  # ✅ This line is essential
 
 def book_list(request):
     books = Book.objects.all()
