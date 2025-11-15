1a4eb8ad4dba48a00463244fcbc03adfe13d3b5f
myla48 <ruhamazerihun2@gmail.com>
2025-11-16 01:10:19 +0300

    Update models.py

--- advanced_features_and_security/LibraryProject/bookshelf/models.py
+++ advanced_features_and_security/LibraryProject/bookshelf/models.py
@@ -1,33 +1,17 @@
-from django.contrib.auth.models import AbstractUser, BaseUserManager
 from django.db import models
 
+class Book(models.Model):
+    """Book model with custom permissions for Task 1."""
 
-class CustomUserManager(BaseUserManager):
-    """Custom manager for CustomUser with email as unique identifier."""
+    title = models.CharField(max_length=200)
+    author = models.CharField(max_length=100)
+    published_date = models.DateField(null=True, blank=True)
 
-    def create_user(self, username, email, password=None, **extra_fields):
-        if not email:
-            raise ValueError("Users must have an email address")
-        email = self.normalize_email(email)
-        user = self.model(username=username, email=email, **extra_fields)
-        user.set_password(password)
-        user.save(using=self._db)
-        return user
-
-    def create_superuser(self, username, email, password=None, **extra_fields):
-        extra_fields.setdefault("is_staff", True)
-        extra_fields.setdefault("is_superuser", True)
-        return self.create_user(username, email, password, **extra_fields)
-
-
-class CustomUser(AbstractUser):
-    """Custom user model extending Django's AbstractUser."""
-
-    email = models.EmailField(unique=True)
-    date_of_birth = models.DateField(null=True, blank=True)
-    profile_photo = models.ImageField(upload_to="profiles/", null=True, blank=True)
-
-    objects = CustomUserManager()
+    class Meta:
+        permissions = [
+            ("can_create", "Can create book"),
+            ("can_delete", "Can delete book"),
+        ]
 
     def __str__(self):
-        return self.username
+        return self.title
