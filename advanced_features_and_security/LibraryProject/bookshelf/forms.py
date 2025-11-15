dec83b53368119d7d7a8905d20627790a57d855a
myla48 <ruhamazerihun2@gmail.com>
2025-11-16 01:17:00 +0300

    Update forms.py

--- advanced_features_and_security/LibraryProject/bookshelf/forms.py
+++ advanced_features_and_security/LibraryProject/bookshelf/forms.py
@@ -12,3 +12,18 @@ class BookForm(forms.ModelForm):
             'author': forms.TextInput(attrs={'class': 'form-control'}),
             'published_date': forms.DateInput(attrs={'class': 'form-control', 'type': 'date'}),
         }
+
+
+class ExampleForm(forms.Form):
+    """Simple example form for demonstrating CSRF protection and security best practices."""
+
+    name = forms.CharField(
+        max_length=100,
+        widget=forms.TextInput(attrs={'class': 'form-control'})
+    )
+    email = forms.EmailField(
+        widget=forms.EmailInput(attrs={'class': 'form-control'})
+    )
+    message = forms.CharField(
+        widget=forms.Textarea(attrs={'class': 'form-control', 'rows': 4})
+    )
