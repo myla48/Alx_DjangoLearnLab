b2881d9d3ca48ab4c512eef7621cbc1b3e76358b
myla48 <ruhamazerihun2@gmail.com>
2025-11-23 21:35:22 +0300

    Update urls.py

--- api_project/api/urls.py
+++ api_project/api/urls.py
@@ -1,6 +1,14 @@
-from django.urls import path
-from .views import BookList
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import BookViewSet, BookList
+
+router = DefaultRouter()
+router.register(r'books_all', BookViewSet, basename='book_all')
 
 urlpatterns = [
+    # Keep the simple list view from Task 1
     path('books/', BookList.as_view(), name='book-list'),
+
+    # Add all CRUD routes from the router
+    path('', include(router.urls)),
 ]
