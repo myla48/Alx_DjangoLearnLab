10dd0483c737b66eae8194b79b3d43294b99c15e
myla48 <ruhamazerihun2@gmail.com>
2025-11-25 12:40:03 +0300

    Update test_views.py

--- advanced-api-project/api/test_views.py
+++ advanced-api-project/api/test_views.py
@@ -1,14 +1,12 @@
 from django.test import TestCase
 from django.urls import reverse
-from rest_framework import status
 from rest_framework.test import APIClient
-from django.contrib.auth.models import User
+from rest_framework import status
 from .models import Author, Book
 
 class BookAPITestCase(TestCase):
     def setUp(self):
         self.client = APIClient()
-        self.user = User.objects.create_user(username="tester", password="testpass123")
         self.author = Author.objects.create(name="John Doe")
         self.book = Book.objects.create(
             title="Test Book",
@@ -19,41 +17,51 @@ class BookAPITestCase(TestCase):
     def test_list_books(self):
         response = self.client.get(reverse('book-list'))
         self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertIn("Test Book", str(response.data))
 
-    def test_create_book_requires_login(self):
-        # Try without login
-        data = {"title": "New Book", "publication_year": 2021, "author": self.author.id}
-        response = self.client.post(reverse('book-create'), data)
-        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+    def test_retrieve_book(self):
+        response = self.client.get(reverse('book-detail', args=[self.book.id]))
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data['title'], "Test Book")
 
-        # Now login
-        login_success = self.client.login(username="tester", password="testpass123")
-        self.assertTrue(login_success)
-
-        # Try again with login
+    def test_create_book(self):
+        data = {
+            "title": "New Book",
+            "publication_year": 2021,
+            "author": self.author.id
+        }
         response = self.client.post(reverse('book-create'), data)
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
-
-    def test_update_book_requires_login(self):
-        data = {"title": "Updated Book", "publication_year": 2022, "author": self.author.id}
-
-        # Without login
-        response = self.client.put(reverse('book-update', args=[self.book.id]), data)
-        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
-
-        # With login
+        self.assertEqual(Book.objects.count(), 2)
         self.client.login(username="tester", password="testpass123")
+
+    def test_update_book(self):
+        data = {"title": "Updated Book", "publication_year": 2022, "author": self.author.id}
         response = self.client.put(reverse('book-update', args=[self.book.id]), data)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.book.refresh_from_db()
         self.assertEqual(self.book.title, "Updated Book")
-
-    def test_delete_book_requires_login(self):
-        # Without login
-        response = self.client.delete(reverse('book-delete', args=[self.book.id]))
-        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
-
-        # With login
         self.client.login(username="tester", password="testpass123")
+
+    def test_delete_book(self):
         response = self.client.delete(reverse('book-delete', args=[self.book.id]))
         self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+        self.assertEqual(Book.objects.count(), 0)
+        self.client.login(username="tester", password="testpass123")
+
+    def test_filter_books_by_year(self):
+        response = self.client.get(reverse('book-list') + '?publication_year=2020')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertIn("Test Book", str(response.data))
+        self.client.login(username="tester", password="testpass123")
+
+    def test_search_books(self):
+        response = self.client.get(reverse('book-list') + '?search=Test')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertIn("Test Book", str(response.data))
+        self.client.login(username="tester", password="testpass123")
+
+    def test_order_books(self):
+        response = self.client.get(reverse('book-list') + '?ordering=title')
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.client.login(username="tester", password="testpass123")
