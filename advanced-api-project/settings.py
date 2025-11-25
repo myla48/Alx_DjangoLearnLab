3de100fe2058ca9df03f133f7d73df74a993fbdf
myla48 <ruhamazerihun2@gmail.com>
2025-11-25 12:15:24 +0300

    Create settings.py

---
+++ advanced-api-project/settings.py
@@ -0,0 +1,23 @@
+INSTALLED_APPS = [
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+
+    # Third-party apps
+    'rest_framework',
+
+    # Your app
+    'api',
+]
+
+# Basic settings
+LANGUAGE_CODE = 'en-us'
+TIME_ZONE = 'UTC'
+USE_I18N = True
+USE_TZ = True
+
+STATIC_URL = 'static/'
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
