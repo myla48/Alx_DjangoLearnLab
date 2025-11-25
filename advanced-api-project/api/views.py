186e1216ff05b870985b36b2b42b614f4fecf9a3
myla48 <ruhamazerihun2@gmail.com>
2025-11-25 12:29:52 +0300

    Update views.py

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -1,4 +1,5 @@
 from rest_framework import generics
+from django_filters import rest_framework
 from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
 from .models import Book
 from .serializers import BookSerializer
