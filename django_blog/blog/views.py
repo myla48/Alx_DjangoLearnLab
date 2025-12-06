4cc024f75099599f1db9d837a42177c5f661887d
myla48 <ruhamazerihun2@gmail.com>
2025-12-06 23:25:04 +0300

    Update views.py

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,5 +1,6 @@
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.contrib.auth.decorators import login_required
 from .models import Post
 
 # List all posts
