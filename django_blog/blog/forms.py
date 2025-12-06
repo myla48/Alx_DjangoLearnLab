1c3b7dc2bd09a1780334b5ed2d427ec53e4073a0
myla48 <ruhamazerihun2@gmail.com>
2025-12-06 23:33:29 +0300

    Update forms.py

--- django_blog/blog/forms.py
+++ django_blog/blog/forms.py
@@ -1,6 +1,15 @@
 from django import forms
 from .models import Post
 from .models import Comment
+from taggit.forms import TagWidget   # 👈 import TagWidget
+
+class PostForm(forms.ModelForm):
+    class Meta:
+        model = Post
+        fields = ["title", "content", "tags"]   # 👈 include tags field
+        widgets = {
+            "tags": TagWidget(),               # 👈 use TagWidget for better tag input
+        }
 
 class CommentForm(forms.ModelForm):
     class Meta:
