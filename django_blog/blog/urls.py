1a068419ef53b45ba0a2c7275eb4aaca093703fa
myla48 <ruhamazerihun2@gmail.com>
2025-12-06 23:24:11 +0300

    Update urls.py

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -15,10 +15,10 @@ urlpatterns = [
     path("post/<int:pk>/delete/", PostDetailView.as_view(), name="post-detail"),
 
     # Create a new post
-    path("posts/new/", PostCreateView.as_view(), name="post-create"),
+    path("post/new/", PostCreateView.as_view(), name="post-create"),
 
     # Update an existing post
-    path("posts/<int:pk>/update/", PostUpdateView.as_view(), name="post-update"),
+    path("post/<int:pk>/update/", PostUpdateView.as_view(), name="post-update"),
 
     # Delete a post
     path("posts/<int:pk>/delete/", PostDeleteView.as_view(), name="post-delete"),
