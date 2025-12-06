b2c9afb340a6d6300950474243d35149e7e6c7ff
myla48 <ruhamazerihun2@gmail.com>
2025-12-06 23:26:37 +0300

    Update forms.py

--- django_blog/blog/forms.py
+++ django_blog/blog/forms.py
@@ -1,5 +1,12 @@
 from django import forms
 from .models import Post
+from .models import Comment
+
+class CommentForm(forms.ModelForm):
+    class Meta:
+        model = Comment
+        fields = ["content"]
+
 
 class PostForm(forms.ModelForm):
     class Meta:
