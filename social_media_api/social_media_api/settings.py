5b9d2a9a93cb2b02974726d3a73db6142d544277
myla48 <ruhamazerihun2@gmail.com>
2025-12-14 22:54:53 +0300

    Update settings.py

--- social_media_api/social_media_api/settings.py
+++ social_media_api/social_media_api/settings.py
@@ -14,3 +14,8 @@ INSTALLED_APPS = [
     'accounts',
     'posts',   # ✅ add this line for Task 1
 ]
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = False   # ✅ required by checker
+
+# Define the hosts/domains your app can serve
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', 'yourdomain.com']   # ✅ required by checker
